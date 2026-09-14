7c1bbd47fc4286e271adf7116fcfb476d8d2e764
Mackenzie Neill <mackenzie.neill.730@gmail.com>
2026-09-14 11:50:51 -0400

    Lazy loading fix
    When there are issues with communicating with the servers, workable components are returned instead of notFound(); flags that get picked up by the root app

--- frontend/src/shared/components/LazyLoad.tsx
+++ frontend/src/shared/components/LazyLoad.tsx
@@ -1,7 +1,7 @@
-import { notFound } from "next/navigation";
 import { ErrorNotFound } from "../lib/api/errorClasses";
 import { Suspense } from "react";
 import { StateLoadingPage } from "./stateComponents/Loading.states";
+import StateErrorPage from "./stateComponents/Error.states";
 
 interface LazyLoadProps<T>{
    serviceCall: () => Promise<T>;
@@ -25,8 +25,20 @@ async function LazyLoadPage<T>({ serviceCall, children }: Omit<LazyLoadProps<T>,
       response = await serviceCall();
    }
    catch (error) {
-      if (error instanceof ErrorNotFound) { return notFound(); }
-      throw error;
+      if (error instanceof ErrorNotFound) { 
+         return ( 
+            <StateErrorPage>
+               <p>404 - The resources needed to load this component could not be fetched from the server</p>
+            </StateErrorPage> 
+         );
+      }
+      else { 
+         return (
+            <StateErrorPage>
+               <p>500 - There was an unknown issue fetching the resources needed from the server for this component</p>
+            </StateErrorPage>
+         );
+      }
    }
 
    return children(response);
